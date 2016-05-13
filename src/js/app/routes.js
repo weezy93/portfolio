b0961e096e311fa9ea57deb270f10a326d440ef5
Louisa Reese <louisa.e.reese@gmail.com>
2016-05-13 11:13:12 -0600

    initial commit, navbar is done

---
+++ src/js/app/routes.js
@@ -0,0 +1,24 @@
+(function () {
+  angular.module('myApp')
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/home.html'
+      })
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'partials/projects.html'
+      })
+      .state('aboutMe', {
+        url: '/about-me',
+        templateUrl: 'partials/aboutMe.html'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'partials/contact.html'
+      });
+    });
+})();
